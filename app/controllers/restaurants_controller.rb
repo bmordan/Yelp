524fd317a1d3e61f7bf52282f04900b144cbf8fc
Bernard Mordan <bernardmordan@gmail.com>
2014-11-03 15:58:41 +0000

    basic stable set up

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(params[:restaurant].permit(:name))
+    redirect_to restaurants_path
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+end
