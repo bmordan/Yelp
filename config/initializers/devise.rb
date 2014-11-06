e8635cea35f07d5ea46614ddcebe4b39d67486a3
Bernard Mordan <bernardmordan@gmail.com>
2014-11-06 18:59:05 +0000

    ENV for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,9 @@ Devise.setup do |config|
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
   config.omniauth :facebook, Rails.application.secrets.app_key , Rails.application.secrets.app_secret
+
+  config.secret_key = ENV["SECRET_KEY_BASE"]
+
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
